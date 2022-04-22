1a47c73c04cbbb0d6c4bb2279301fb5045b4457d
Hamin Park <haminjp@naver.com>
2022-04-23 00:46:10 +0900

    edit stories

--- src/components/Toggle/Toggle.stories.js
+++ src/components/Toggle/Toggle.stories.js
@@ -5,15 +5,36 @@ export default {
   title: 'Toggle',
 };
 
-const Template = (args) => (
-  <Toggle {...args}>
-    <Toggle.Button label="apple" />
-    <Toggle.Button label="peach" />
-    <Toggle.Button label="banana" />
-    <Toggle.Button label="grass" />
-    <Toggle.Button label="ham" />
+export const TwoButtons = () => (
+  <Toggle>
+    <Toggle.Button label="ON" />
+    <Toggle.Button label="OFF" />
   </Toggle>
 );
 
-export const FiveButtons = Template.bind({});
-FiveButtons.args = {};
+export const ThreeButtons = () => (
+  <Toggle>
+    <Toggle.Button label="Small" />
+    <Toggle.Button label="Medium" />
+    <Toggle.Button label="Large" />
+  </Toggle>
+);
+
+export const FourButtons = () => (
+  <Toggle>
+    <Toggle.Button label="1" />
+    <Toggle.Button label="2" />
+    <Toggle.Button label="3" />
+    <Toggle.Button label="4" />
+  </Toggle>
+);
+
+export const FiveButtons = () => (
+  <Toggle>
+    <Toggle.Button label="Apple" />
+    <Toggle.Button label="Peach" />
+    <Toggle.Button label="Banana" />
+    <Toggle.Button label="Grass" />
+    <Toggle.Button label="Ham" />
+  </Toggle>
+);

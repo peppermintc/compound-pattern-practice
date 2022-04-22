4463b73d79c543d5f0e5399c1681288d33edf60d
Hamin Park <haminjp@naver.com>
2022-04-22 13:48:38 +0900

    add toggle stories

---
+++ src/components/Toggle/Toggle.stories.js
@@ -0,0 +1,19 @@
+import Toggle from './Toggle';
+
+export default {
+  component: Toggle,
+  title: 'Toggle',
+};
+
+const Template = (args) => (
+  <Toggle {...args}>
+    <Toggle.Button label="apple" />
+    <Toggle.Button label="peach" />
+    <Toggle.Button label="banana" />
+    <Toggle.Button label="grass" />
+    <Toggle.Button label="ham" />
+  </Toggle>
+);
+
+export const fiveButtons = Template.bind({});
+fiveButtons.args = {};
